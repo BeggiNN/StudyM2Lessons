721d994a7ea0a4971c6e46eaa792c122d38eaa1c
Vanya <ggflappygg@gmail.com>
2021-08-09 16:12:33 +0300

    update

--- app/code/Perspective/UiComponent/registration.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-use Magento\Framework\Component\ComponentRegistrar;
-
-ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Perspective_UiComponent', __DIR__);
--- app/code/Perspective/UiComponent/view/frontend/requirejs-config.js
+++
@@ -1,9 +0,0 @@
-var config = {
-    config: {
-        mixins: {
-            'Magento_Checkout/js/view/payment/default': {
-                'Perspective_UiComponent/js/checkout/view/shipping/default-mixin': true
-            }
-        }
-    }
-};
